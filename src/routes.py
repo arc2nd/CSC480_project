48d0e7781918bb0d898ddc8159a91738b16ffe8d
James Parks <arcsecond@gmail.com>
2018-12-04 20:48:19 -0800

    cleaned up routes to remove confusing unused stuff

--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import json
-import datetime
-import calendar
-import commands
-from functools import wraps
 
 from flask import Flask, render_template, Response, redirect, url_for, request, session, flash
 
@@ -19,50 +12,18 @@ def _log(priority, msg):
         print(msg)
 
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        for i in session:
-            _log(6, i)
-        if 'logged_in' not in session:
-            return redirect(url_for('login')) #, next=request.url))
-        return f(*args, **kwargs)
-    return decorated_function
-
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
-#@login_required
 def index():
     return render_template('index.html')
 
-
-#@app.route('/', methods=['GET', 'POST'])
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login')
 def login():
-    form = forms.EmailPasswordForm()
-    if form.validate_on_submit():
-        new_login = user.User(name=form.email.data)
-        _log(1, form.password.data)
-        if new_login.verify(passwd=form.password.data):
-            ts = calendar.timegm(datetime.datetime.now().timetuple())
-            session['user'] = new_login.to_dict()
-            session['logged_in'] = ts
-            for i in session:
-                _log(6, i)
-            return redirect(url_for('index'))
-        flash('wrong password')
-    return render_template('login.html', form=form, footer='False')
+    return render_template('login.html')
 
-
-@app.route('/logout', methods=['GET'])
+@app.route('/logout')
 def logout():
-    user_name = session['user']['name']
-    session.pop('user')
-    for i in session:
-        _log(6, i)
-    return render_template('logout.html', user=user_name)
-
+    return render_template('logout.html')
 
 @app.route('/chores', methods=['GET'])
 def chores():
