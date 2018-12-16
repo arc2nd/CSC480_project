4756fd0023f83e9830a96864cdee3045f99f80be
James Parks <arcsecond@gmail.com>
2018-12-16 15:27:05 -0800

    added a form example. doesn\'t really do anything yet

--- src/Helpers.py
+++ src/Helpers.py
@@ -1,3 +1,57 @@
 #!/usr/bin/env python
 
 #this is a good spot to put functions that don't really fit other descriptions
+
+MANDATORY_CHORE_KEYS = ['name']
+OPTIONAL_CHORE_KEYS= ['points', 'desc', 'due']
+POSSIBLE_CHORE_KEYS = MANDATORY_CHORE_KEYS + OPTIONAL_CHORE_KEYS
+
+MANDATORY_REWARD_KEYS = ['name', 'points']
+OPTIONAL_REWARD_KEYS = ['desc', 'expires']
+POSSIBLE_REWARD_KEYS = MANDATORY_REWARD_KEYS + OPTIONAL_REWARD_KEYS
+
+MANDATORY_USER_KEYS = ['name', 'email', 'type']
+OPTIONAL_USER_KEYS = ['first', 'middle', 'last', 'dob', 'style']
+POSSIBLE_USER_KEYS = MANDATORY_USER_KEYS + OPTIONAL_USER_KEYS
+
+
+def _convert_form_keys(in_dict):
+    """Converts an ImmutableDict to a regular Dict"""
+    ret_dict = {}
+    for key in in_dict:
+        ret_dict[key] = in_dict[key]
+    return ret_dict
+
+def _validate_form_keys(in_dict, mandatory_list):
+    """Make sure that the mandatory keys have values"""
+    missing = []
+    for key in in_dict:
+        if key in mandatory_list:
+            print('key is mandatory: {}'.format(key))
+            if not in_dict[key]:
+                print(key)
+                missing.append(key)
+    return missing
+
+
+def _process_chore_form(res):
+    """convert chore form to Dict and find missing keys"""
+    ret_dict = _convert_form_keys(res)
+    missing = _validate_form_keys(ret_dict, MANDATORY_CHORE_KEYS)
+    return ret_dict, missing
+
+        
+def _process_reward_form(res):
+    """convert reward form to Dict and find missing keys"""
+    ret_dict = _convert_form_keys(res)
+    missing = _validate_form_keys(ret_dict, MANDATORY_REWARD_KEYS)
+    return ret_dict, missing
+
+
+def _process_user_form(res):
+    """convert user form to Dict and find missing keys"""
+    ret_dict = _convert_form_keys(res)
+    missing = _validate_form_keys(ret_dict, MANDATORY_USER_KEYS)
+    return ret_dict, missing
+
+
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+import json
 
 from flask import Flask, render_template, Response, redirect, url_for, request, session, flash
+
+import Helpers
+
 
 app = Flask(__name__)
 
@@ -11,6 +15,7 @@ def _log(priority, msg):
     if VERBOSITY >= priority:
         print(msg)
 
+# The actual website endpoints
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
@@ -25,7 +30,7 @@ def login():
 def logout():
     return render_template('logout.html')
 
-@app.route('/chores', methods=['GET'])
+@app.route('/chores', methods=['GET', 'POST'])
 def chores():
     chores_dict = {'Take out trash': 1, 
                    'Take out recycling': 1, 
@@ -38,6 +43,16 @@ def chores():
                    'Vacuum': 1}
     return render_template('chores.html', data=chores_dict)
 
+@app.route('/new_chore_results', methods=['GET', 'POST'])
+def new_chore_results():
+    res = request.form
+    processed_res, missing = Helpers._process_chore_form(res)
+    _log(1, missing)
+    if len(missing) > 0:
+        return render_template('error.html', data=missing)
+    res_str = json.dumps(processed_res, sort_keys=True, indent=4)
+    _log(1, res_str)
+    return render_template('new_chore_results.html', data=res)
 
 @app.route('/available', methods=['GET'])
 def available():
@@ -52,6 +67,12 @@ def new_chore():
 @app.route('/my_account', methods=['GET'])
 def my_account():
     return render_template('my_account.html')
+
+
+@app.route('/error', methods=['GET'])
+def error():
+    return render_template('error.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
