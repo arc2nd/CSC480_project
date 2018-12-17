1c59e80a34a783db21715c472ad0d346ad66428c
James Parks <arcsecond@gmail.com>
2018-12-16 21:29:02 -0800

    added temporary json storage for chores

--- src/db.py
+++ src/db.py
@@ -2,3 +2,46 @@
 
 # this is probably a good place to put database related functions
 # unless we want a database object
+
+#I've temporarly put in some functions to read and write to JSON files
+#Could use some more try/except blocks around the file access
+
+
+import os
+import json
+
+CONN = os.path.join(os.getcwd(), 'chore_storage')
+
+def _validate_storage_location(conn):
+    ret_val = True
+    if not os.path.exists(conn):
+        os.mkdir(conn)
+        ret_val = False
+    return ret_val
+
+
+def _write_to_storage(conn, data):
+    ret_val = False
+    valid = _validate_storage_location(conn)
+    with open(os.path.join(conn, "{}.json".format(data['name'].replace(' ', '_'))), 'w') as fp:
+        json.dump(data, fp, indent=4, sort_keys=True)
+        ret_val = True
+    return ret_val
+
+
+def _read_from_storage(conn, target):
+    ret_val = False
+    data_dict = {}
+    valid = _validate_storage_location(conn)
+    with open(os.path.join(conn, '{}.json'.format(target)), 'r') as fp:
+        data_dict = json.load(fp)
+        ret_val = True
+    return ret_val, data_dict
+
+def get_available_chores(conn):
+    all_chores = []
+    if os.path.exists(conn):
+        all_chores = os.listdir(conn)
+    return all_chores
+
+    
--- src/routes.py
+++ src/routes.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
+import os
 import json
 
 from flask import Flask, render_template, Response, redirect, url_for, request, session, flash
 
+import db
 import Helpers
 
 
 app = Flask(__name__)
 
+# set some globals
 VERBOSITY = 1
+DB_CONN = db.CONN
 
+
+# it'd probably be a good idea to put this into a log.py file
+# or, you know, use a real logging scheme
 def _log(priority, msg):
     if VERBOSITY >= priority:
         print(msg)
+
 
 # The actual website endpoints
 
@@ -30,9 +38,10 @@ def login():
 def logout():
     return render_template('logout.html')
 
+
 @app.route('/chores', methods=['GET', 'POST'])
 def chores():
-    chores_dict = {'Take out trash': 1, 
+    chores_dict = {'Take out trash': 1,  # build a fake chore dict
                    'Take out recycling': 1, 
                    'Clean kitty litter': 2, 
                    'Clean chicken coop': 3, 
@@ -43,21 +52,35 @@ def chores():
                    'Vacuum': 1}
     return render_template('chores.html', data=chores_dict)
 
+
 @app.route('/new_chore_results', methods=['GET', 'POST'])
 def new_chore_results():
-    order_list = ['name', 'desc', 'assigned_to', 'points', 'due']
     res = request.form
-    processed_res, missing = Helpers._process_chore_form(res)
-    _log(1, missing)
+    if 'cancel' in res:  # check to see if the cancel button has been pressed
+        return redirect(url_for('index'))
+    _log(6, res)
+    processed_res, missing = Helpers._process_chore_form(res)  # process the form results and validate
+    _log(6, missing)
     if len(missing) > 0:
         return render_template('error.html', data=missing)
+    db._write_to_storage(DB_CONN, processed_res)  # write chore to db (currently json storage)
     res_str = json.dumps(processed_res, sort_keys=True, indent=4)
-    _log(1, res_str)
+    _log(6, res_str)
+    order_list = ['name', 'desc', 'assigned_to', 'points', 'due']
     return render_template('new_chore_results.html', order=order_list, data=res)
+
 
 @app.route('/available', methods=['GET'])
 def available():
-    return render_template('available.html')
+    chores_list = db.get_available_chores(DB_CONN)  # get chores from db (currently a list of json files)
+    chores_dict = {}
+    for chore in chores_list:  # build a large dict with all of the info in it
+        with open(os.path.join(DB_CONN, chore), 'r') as fp:
+            this_chore = json.load(fp)
+        chores_dict[chore] = this_chore
+    order_list = [k for k in chores_dict]  # get keys(chore names)
+    order_list.sort()  # sort the name list, can change sort type
+    return render_template('available.html', order=order_list, data=chores_dict)
 
 
 @app.route('/new_chore', methods=['GET'])
@@ -77,4 +100,5 @@ def error():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
 
