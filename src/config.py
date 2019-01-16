a2a2ebc7bc3f1cca7e64671b44fd4afcfaf95c31
James Boyer <aesthetics1@gmail.com>
2019-01-16 11:57:21 -0800

    Put creds fallback back in

--- src/config.py
+++ src/config.py
@@ -11,6 +11,14 @@ def get_now():
     return calendar.timegm(datetime.datetime.now().timetuple())
 
 def get_creds(path, crypt=False):
+    ret_dict = { 
+                "ADMIN_ROLE_ID": 0,
+				"SECRET_KEY": "I am a secret key", 
+                "CSRF_ENABLED": True, 
+                "SQLALCHEMY_DATABASE_URI": "postgresql://cxp:choresarereallyfun@localhost/ChoreExplore", 
+                "SQLALCHEMY_TRACK_MODIFICATIONS": False, 
+                "WTF_CSRF_SECRET_KEY": "this-needs-to-change-in-production"
+               }
     if crypt:
         # this section must be replaced with a more cross-platform encrypt/decrypt function
         cmd = "openssl des3 -salt -d -in %s -pass pass:%s" % (path, os.path.basename(path))
