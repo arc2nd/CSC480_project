a6a16ea814c73983af9ab075e52b0d969c38f881
jboyer87 <aesthetics1@gmail.com>
2019-01-23 20:22:28 -0800

    Incremented version, removed unused config dictionary

--- src/config.py
+++ src/config.py
@@ -11,33 +11,8 @@ def get_now():
     return calendar.timegm(datetime.datetime.now().timetuple())
 
 def get_creds(path, crypt=False):
-    ret_dict = { 
-                "ADMIN_ROLE_ID": 1,
-                "APPLICATION_VERSION": "1.1.0",
-                "CSRF_ENABLED": True, 
-                "STANDARD_ROLE_ID": 2,
-                "SECRET_KEY": "I am a secret key", 
-                "SQLALCHEMY_DATABASE_URI": "postgresql://cxp:choresarereallyfun@localhost/ChoreExplore", 
-                "SQLALCHEMY_TRACK_MODIFICATIONS": False, 
-                "WTF_CSRF_SECRET_KEY": "this-needs-to-change-in-production"
-               }
-    if crypt:
-        # this section must be replaced with a more cross-platform encrypt/decrypt function
-        cmd = "openssl des3 -salt -d -in %s -pass pass:%s" % (path, os.path.basename(path))
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        output = proc.communicate()[0]
-        if (output):
-            try:
-                j = json.loads(output)
-                return j
-            except:
-                return ret_dict 
-    else:
-        path = os.path.join(basedir, path)
-        if os.path.exists(path):
-            try:
-                with open(path, 'r') as fp: 
-                    j = json.load(fp)
-                return j
-            except:
-                return ret_dict
+    path = os.path.join(basedir, path)
+    if os.path.exists(path):
+        with open(path, 'r') as fp: 
+            j = json.load(fp)
+        return j
